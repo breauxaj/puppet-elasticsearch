87d21b2a89c5481ca79bb937b3065dbe61ad8627
Adam Breaux <breauxaj@gmail.com>
2015-08-23 17:38:18 -0400

    conversion

--- manifests/sysconfig.pp
+++ manifests/sysconfig.pp
@@ -1,5 +1,18 @@
 define elasticsearch::sysconfig (
-
+  $data_dir = undef,
+  $log_dir = undef,
+  $work_dir = undef,
+  $es_heap_size = undef,
+  $es_heap_newsize = undef,
+  $es_direct_size = undef,
+  $es_java_opts = undef,
+  $es_restart_on_upgrade = undef,
+  $es_gc_log_file = undef,
+  $es_user = undef,
+  $es_group = undef,
+  $max_open_files = undef,
+  $max_locked_memory = undef,
+  $max_map_count = undef
 ) {
   include ::elasticsearch
 
