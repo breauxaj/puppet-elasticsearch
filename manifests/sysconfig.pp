7b9bd682b2eab6c57e74e8add57417e1d2d89dce
Adam Breaux <breauxaj@gmail.com>
2015-08-23 18:01:34 -0400

    do it the smart way
    its just an INI file

--- manifests/sysconfig.pp
+++ manifests/sysconfig.pp
@@ -1,31 +1,20 @@
 define elasticsearch::sysconfig (
-  $data_dir = undef,
-  $log_dir = undef,
-  $work_dir = undef,
-  $es_heap_size = undef,
-  $es_heap_newsize = undef,
-  $es_direct_size = undef,
-  $es_java_opts = undef,
-  $es_restart_on_upgrade = undef,
-  $es_gc_log_file = undef,
-  $es_user = undef,
-  $es_group = undef,
-  $max_open_files = undef,
-  $max_locked_memory = undef,
-  $max_map_count = undef
+  $value
 ) {
   include ::elasticsearch
 
-  $conf = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => '/etc/sysconfig',
+  $key = $title
+
+  $context = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      '/files/etc/sysconfig/elasticsearch'
+    ],
   }
 
-  file { "${conf}/elasticsearch":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('elasticsearch/sysconfig.erb'),
+  augeas { "sysconfig_elasticsearch/${key}":
+    context => $context,
+    onlyif  => "get ${key} != '${value}'",
+    changes => "set ${key} '${value}'",
   }
 
 }
